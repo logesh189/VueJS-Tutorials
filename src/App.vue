c84e3cadb2ad7da1e4a7c599aaf03d3c45ebca5c
logesh189 <42236568+logesh189@users.noreply.github.com>
2021-10-03 16:34:35 +0530

    Form Cmp Changes

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,42 @@
 <template>
   <div id="app">
+    <FormComponent>
+      <div slot="form-header">
+        <h2>
+          Signup Form
+        </h2>
+        <p>
+          Please fill out the form
+        </p>
+      </div>
+      <div slot="form-fields">
+        <input type="text" class="form-input" placeholder="First name" />
+        <input type="text" class="form-input" placeholder="Last name" />
+        <input type="text" class="form-input" placeholder="Mail Id" />
+        <input type="Password" class="form-input" placeholder="Password" />
+      </div>
+      <div slot="form-actions">
+        <button class="btn btn-success">
+          SUBMIT
+        </button>
+      </div>
+      <div slot="form-links">
+        <ul>
+          <li><a href="#">Sign in,  </a></li>
+          <li><a href="#">Forgot Password</a></li>
+        </ul>
+      </div>
+    </FormComponent>
   </div>
 </template>
 
 <script>
+import FormComponent from "./components/FormComponent";
 
 export default {
   name: "App",
   components: {
+    FormComponent
   }
 };
 </script>
---
+++ src/components/FormComponent.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <form>
+      <div id="form-header">
+        <slot name="form-header"></slot>
+      </div>
+      <div id="form-field">
+        <slot name="form-fields"></slot>
+      </div>
+      <div id="form-links">
+        <slot name="form-links"></slot>
+      </div>
+      <div id="form-actions">
+        <slot name="form-actions"></slot>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "FormComponent",
+    data() {
+      return {
+
+      }
+    },
+  }
+</script>
